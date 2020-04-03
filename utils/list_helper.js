595c35c36c22ced0329ca1b50cd910c77f66f63c
yasir <aff3ad@gmail.com>
2020-04-03 18:02:45 +0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,23 @@ const dummy = (blogs) => {
     // ...
     return 1
   }
+
+const totalLikes = (blog_post) => {
+  if(blog_post.length > 0){
+    const getLikes = []
+    const likes =  blog_post.forEach(post => {
+      getLikes.push(post.likes)
+    });
+    return getLikes.reduce((acc,getLikes) => acc+getLikes, 0)
+  } else {
+    return 0
+  }
+}
+
+
+
+
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
