551c34a14d9c597f2fe5b13582744b8a92756e5a
yasir <aff3ad@gmail.com>
2020-04-04 08:52:51 +0300

    4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 const dummy = (blogs) => {
     // ...
     return 1
@@ -14,11 +15,27 @@ const totalLikes = (blog_post) => {
     return 0
   }
 }
+const favoriteBlog = (blogs) => {
+  const getLikes = []
+  const likes =  blogs.forEach(post => {
+    getLikes.push(post.likes)
+  });
+  const highestVote = Math.max(...getLikes)
+  let keyPosition = Object.keys(blogs).find(key => blogs[key].likes === highestVote);
+  const postWithMostLike = blogs[keyPosition]
+  delete postWithMostLike._id && delete postWithMostLike.url && delete postWithMostLike.__v
+  return postWithMostLike
+}
 
-
-
-
-  
+const mostBlogs = (blogs) => {
+  const getAllAuthors = _.countBy(blogs, function(o) { return o.author; })
+  const authorName = _.maxBy(_.keys(getAllAuthors), function (o) { return getAllAuthors[o]; });
+  const authorWithNumberOfBlog = _.pick(getAllAuthors,authorName) 
+  var author = {}
+  author['author'] = authorName
+  author['blogs'] = authorWithNumberOfBlog[authorName]
+  return author
+}
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes,favoriteBlog, mostBlogs
   }
