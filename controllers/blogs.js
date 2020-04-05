dc96092a0c0898991e813e1b3df952364b929816
yasir <aff3ad@gmail.com>
2020-04-05 17:20:55 +0300

    4.13 Blog list expansions, step1 + 4.14 step2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -15,4 +15,19 @@ blogRouter.post('/', async(request, response) => {
     response.json(savedNote.toJSON())
   })
 
+  blogRouter.delete('/:id', async(request, responce) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    responce.status(204).end()
+  })
+
+  blogRouter.put('/:id', async(request, responce) => {
+    const getBody = request.body
+    const blog = {
+      title: getBody.title, author:getBody.author, url:getBody.url, likes:getBody.likes
+    }
+    const getBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new:true})
+    responce.json(getBlog.toJSON())
+
+  })
+
   module.exports = blogRouter
